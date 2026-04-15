07c8be9cfaa4a3c03cca6a0ef4652b230dd32ffb
Amun Jackson <amun.jackson@example.com>
2026-04-15 16:15:04 -0400

    Adding Server

--- frontend/senior-project/vite.config.js
+++ frontend/senior-project/vite.config.js
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5004",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
     },
   },
 });
